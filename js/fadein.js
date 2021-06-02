084b7e9822578776795fc6522595a9534c539c6e
okawidiawan <oka.widiawan@gmail.com>
2021-06-03 05:51:43 +0700

    initial commit

---
+++ js/fadein.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    $('h1.hidden').fadeIn(1000).removeClass('hidden');
+});
+
+$(document).ready(function () {
+    $('ul.hidden').fadeIn(1000).removeClass('hidden');
+});
---
+++ js/sidenav.js
@@ -0,0 +1,10 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "80px";
+    document.getElementById("main").style.marginLeft = "80px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+    document.body.style.backgroundColor = "white";
+}
