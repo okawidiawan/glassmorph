dd11c5aa5e851709c4649687f29e8e5ac55fcc0b
Oka Widiawan <oka.widiawan@gmail.com>
2021-10-28 12:43:06 +0700

    New CV template

--- js/fadein.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-    $('h1.hidden').fadeIn(1000).removeClass('hidden');
-});
-
-$(document).ready(function () {
-    $('ul.hidden').fadeIn(1000).removeClass('hidden');
-});
